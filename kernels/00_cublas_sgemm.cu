04b978ec50afc973b35195815e792fd4bb1abe23
trainer_brendan <omkarpast@gmail.com>
2025-02-12 15:40:21 +0530

    Update 00_cublas_sgemm.cu

--- kernels/00_cublas_sgemm.cu
+++ kernels/00_cublas_sgemm.cu
@@ -5,7 +5,6 @@ void run_cublas_sgemm(cublasHandle_t handle, int M, int N, int K,
                       float alpha, float* d_A, float* d_B, 
                       float beta, float* d_C) {
 
-    cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, &alpha, d_B, CUDA_R_32F,
-               N, d_A, CUDA_R_32F, K, &beta, d_C, CUDA_R_32F, N,
-               CUBLAS_COMPUTE_32F_FAST_TF32, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+    cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, N, M, K, 
+                &alpha, d_B, N, d_A, K, &beta, d_C, N);
 }
